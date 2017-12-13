b15ef46cb3e42031a50fbd23e112342d6d4f6385
0rC0 <orco@paranoici.org>
2017-12-13 23:27:46 +0100

    ueb6

--- ueb6/generateData.m
+++ ueb6/generateData.m
@@ -1,4 +1,5 @@
 function y = generateData(x)
+	format long
 	%f^(-1) wird f2 genannt
 	%val1 ist die verkettung f2(f1(x)) 
 	%val2 ist die verkettung f1(f2(x))
@@ -7,7 +8,7 @@ function y = generateData(x)
 	f2 = @(x) sqrt(x+1)-1;
 	val1 = concat(f2, f1, x);
 	val2 = concat(f1, f2, x);
-	err1 = (x - v1) / x;
-	err2 = (x - v2) / x;
+	err1 = (x - val1) / x;
+	err2 = (x - val2) / x;
 	y = [val1, val2, err1, err2];
 end
--- ueb6/run.m
+++ ueb6/run.m
@@ -1,5 +1,6 @@
 % run.m
-format long #wir brauchen zahlen wie -1 * 10^12
+%wir brauchen zahlen wie -1 * 10^12
+format long 
 k = [0:12]
 f = @(x) (-1 + 10.^x)
 erg = []; %ergebnisse matrix
@@ -8,5 +9,11 @@ for i=k
 	erg = cat(1,tmp,erg);
 end
 
-%ToDo: export as csv
+%Exportiere als csv
+%Ref: https://de.mathworks.com/help/matlab/ref/csvwrite.html?s_tid=gn_loc_drop
+csvwrite('daten.txt', erg);
 
+%Tabelledarstellung
+%Ref: https://de.mathworks.com/help/matlab/ref/array2table.html
+
+array2table(erg, 'VariableNames',{'k','val1','val2', 'err1', 'err2'})
