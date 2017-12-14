d9811ae85bcc200d1e04bf46068a4be31f2d6190
0rC0 <orco@paranoici.org>
2017-12-14 12:43:42 +0100

    ueb6

--- ueb6/concat.m
+++ ueb6/concat.m
@@ -1,4 +1,8 @@
-function y = concat(g1, g2, x)
+function y = concat(g1, g2)
     % Verkettung zweier Funktionen g1 und g2
-    y = g1(g2(x));
+    % Bsp: g1 = @(x) x.^2
+    %      g2 = @(x) sin(x)
+    %      g3 = concat(g1, g2)
+    %      g3(pi/2) % ans = 1
+    y = @(x) g1(g2(x));
 end
--- ueb6/generateData.m
+++ ueb6/generateData.m
@@ -6,8 +6,10 @@ function y = generateData(x)
 	% Die relative fehlern (x - val1(bzw v2)) / x
 	f1 = @(x) x*(x+2);
 	f2 = @(x) sqrt(x+1)-1;
-	val1 = concat(f2, f1, x);
-	val2 = concat(f1, f2, x);
+	f21 = concat(f2, f1);
+	f12 = concat(f1, f2);
+	val1 = f21(x);
+	val2 = f12(x);
 	err1 = (x - val1) / x;
 	err2 = (x - val2) / x;
 	y = [val1, val2, err1, err2];
